e2b3814031ec5bfeb21e6812afb5dc674fc8a807
Said Tursunkulov <ruk4816@gmail.com>
2026-01-30 10:19:23 +0500

    Lab 1: image processing using OpenCV

---
+++ main.py
@@ -0,0 +1,38 @@
+# import cv2
+
+
+# color_image = cv2.imread('download.jpeg')
+
+
+# cv2.imshow('Color Image', color_image)
+
+# print("Image shape:", color_image.shape)
+
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# # import cv2
+
+# # gray1 = cv2.imread('download.jpeg', cv2.IMREAD_GRAYSCALE)
+
+# # cv2.imshow('Grayscale Method 1', gray1)
+# # cv2.imwrite('grayscale_method1.jpg', gray1)
+
+# # cv2.waitKey(0)
+# # cv2.destroyAllWindows()
+
+
+# import cv2
+
+# color = cv2.imread('download.jpeg')
+# gray = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
+
+# _, binary_128 = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY)
+
+# cv2.imshow('Binary 128', binary_128)
+# cv2.imwrite('binary_128.jpg', binary_128)
+
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
